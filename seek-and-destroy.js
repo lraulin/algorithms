35fbfaa20c8b6a646ffce0473c48b6fe65a5acd9
Lee Raulin <leeraulin@gmail.com>
2018-04-28 21:40:50 -0400

    ...

--- seek-and-destroy.js
+++ seek-and-destroy.js
@@ -1,10 +1,11 @@
 function destroyer(arr) {
-  const args = arguments.slice(1);
-  for (arg of args) {
-    arr = arr.filter(x => x == arg)
+  let output = arguments[0];
+  for (let index = 1; index < arguments.length; index++) {
+    output = output.filter(x => x != arguments[index]);
   }
-  return arr;
+  return output;
 }
 
 const answer = destroyer([1, 2, 3, 1, 2, 3], 2, 3);
-console.log(answer);
+const answer2 = destroyer([3, 5, 1, 2, 2], 2, 3, 5);
+console.log(answer2);
