29088fc6af85362958e5b9ffc86fa94749f957f3
Lee Raulin <leeraulin@gmail.com>
2018-05-06 13:05:00 -0400

    seek and destroy

---
+++ seek_and_destroy.py
@@ -0,0 +1,9 @@
+# Unlike JS, Python throws TypeError if you give a function extra parameters.
+def destroyer(arr, *args):
+    for arg in args:
+        arr = [x for x in arr if x != arg]
+    print(arr)
+    return arr
+
+
+destroyer([3, 5, 1, 2, 2], 2, 3, 5)
