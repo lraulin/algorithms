d6d6457d69585d83ab60a9dbd01d84d84532de6f
= <leeraulin@gmail.com>
2018-07-13 15:56:26 -0400

    refactor longest-consecutive with reduce

--- js/medium/longest-consecutive.js
+++ js/medium/longest-consecutive.js
@@ -23,28 +23,49 @@
 function longestConsecutive(arr) {
   // sort without mutating
   const sequence = [].concat(arr).sort((a, b) => a - b);
+
   // length of current sequence
-  let currentCount = 1;
+  // let currentCount = 1;
   // length of longest sequence
-  let maxCount = 1;
+  // let maxCount = 1;
   // compare values it indexes i and i + 1
   // for the last pair, i+1 is out of range, therefore undefined, therefore
   // will be treated as the end of a consecutive sequence
-  for (let i = 0; i < sequence.length; i++) {
-    // if the next num equals current num + 1...
-    if (sequence[i + 1] === sequence[i] + 1) {
-      // the pairs are part of a consecutive sequence; increase the count
-      currentCount++;
-    } else {
-      // we've reached the end of a consecutive sequence
-      // update maxCount if appropriate
-      maxCount = currentCount > maxCount ? currentCount : maxCount;
-      // reset currentCount
-      currentCount = 1;
-    }
-  }
-  return maxCount;
+  // for (let i = 0; i < sequence.length; i++) {
+  //   // if the next num equals current num + 1...
+  //   if (sequence[i + 1] === sequence[i] + 1) {
+  //     // the pairs are part of a consecutive sequence; increase the count
+  //     currentCount++;
+  //   } else {
+  //     // we've reached the end of a consecutive sequence
+  //     // update maxCount if appropriate
+  //     maxCount = currentCount > maxCount ? currentCount : maxCount;
+  //     // reset currentCount
+  //     currentCount = 1;
+  //   }
+  // }
+  // return maxCount;
+
+  // figured out how to do it with reduce instead of a for loop, but I'm not
+  // sure it's easier to read or reason about in this case
+  const obj = sequence.reduce(
+    (acc, current) => {
+      if (current === acc.prev + 1) {
+        acc.currentCount++;
+        acc.maxCount =
+          acc.currentCount > acc.maxCount ? acc.currentCount : acc.maxCount;
+      } else {
+        acc.currentCount = 1;
+      }
+      acc.prev = current;
+      return acc;
+    },
+    { prev: null, currentCount: 0, maxCount: 1 }
+  );
+  return obj.maxCount;
 }
 
 const test = [4, 3, 8, 1, 2, 6, 100, 9];
+// const test = [6, 7, 3, 1, 100, 102, 6, 12];
+// const test = [5, 6, 1, 2, 8, 9, 7];
 console.log(longestConsecutive(test));
