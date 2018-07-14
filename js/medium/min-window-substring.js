7d7c7e6319ece382541d50132e1001a5dcf6fcd1
= <leeraulin@gmail.com>
2018-07-14 17:36:34 -0400

    add triple double

--- js/medium/min-window-substring.js
+++ js/medium/min-window-substring.js
@@ -1,28 +1,71 @@
 /**
- * Challenge 
- * Using the JavaScript language, have the function MinWindowSubstring(strArr) 
- * take the array of strings stored in strArr, which will contain only two 
- * strings, the first parameter being the string N and the second parameter 
- * being a string K of some characters, and your goal is to determine the 
- * smallest substring of N that contains all the characters in K. For example: 
- * if strArr is ["aaabaaddae", "aed"] then the smallest substring of N that 
- * contains the characters a, e, and d is "dae" located at the end of the 
- * string. So for this example your program should return the string dae. 
- * 
- * Another example: if strArr is ["aabdccdbcacd", "aad"] then the smallest 
- * substring of N that contains all of the characters in K is "aabd" which is 
- * located at the beginning of the string. Both parameters will be strings 
- * ranging in length from 2 to 50 characters and all of K's characters will 
- * exist somewhere in the string N. Both strings will only contains lowercase 
- * alphabetic characters. 
- * Sample Test Cases 
- * Input:"ahffaksfajeeubsne", "jefaa" 
- * 
- * Output:"aksfaje" 
- * 
- * 
- * Input:"aaffhkksemckelloe", "fhea" 
- * 
- * Output:"affhkkse" 
+ * Challenge
+ * Using the JavaScript language, have the function MinWindowSubstring(strArr)
+ * take the array of strings stored in strArr, which will contain only two
+ * strings, the first parameter being the string N and the second parameter
+ * being a string K of some characters, and your goal is to determine the
+ * smallest substring of N that contains all the characters in K. For example:
+ * if strArr is ["aaabaaddae", "aed"] then the smallest substring of N that
+ * contains the characters a, e, and d is "dae" located at the end of the
+ * string. So for this example your program should return the string dae.
+ *
+ * Another example: if strArr is ["aabdccdbcacd", "aad"] then the smallest
+ * substring of N that contains all of the characters in K is "aabd" which is
+ * located at the beginning of the string. Both parameters will be strings
+ * ranging in length from 2 to 50 characters and all of K's characters will
+ * exist somewhere in the string N. Both strings will only contains lowercase
+ * alphabetic characters.
+ * Sample Test Cases
+ * Input:"ahffaksfajeeubsne", "jefaa"
+ *
+ * Output:"aksfaje"
+ *
+ *
+ * Input:"aaffhkksemckelloe", "fhea"
+ *
+ * Output:"affhkkse"
  */
 
+// naive/brute force
+String.prototype.containsLetters = function(str) {
+  function countLetters(s) {
+    const letterCount = {};
+    for (let char of s) {
+      if (letterCount.hasOwnProperty(char)) {
+        letterCount[char]++;
+      } else {
+        letterCount[char] = 1;
+      }
+    }
+    return letterCount;
+  }
+  lettersA = countLetters(this);
+  lettersB = countLetters(str);
+  for (let letter in lettersB) {
+    if (!(lettersA[letter] >= lettersB[letter])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+function minWindowSubstring(strArr) {
+  const word = strArr[0];
+  const letters = strArr[1];
+  let shortestSubstr;
+  for (let i = 0; i < word.length; i++) {
+    for (let j = i + 1; j < word.length + 1; j++) {
+      const sub = word.slice(i, j);
+      if (
+        sub.containsLetters(letters) &&
+        (!shortestSubstr || sub.length < shortestSubstr.length)
+      ) {
+        shortestSubstr = sub;
+      }
+    }
+  }
+  return shortestSubstr;
+}
+
+const test = ["aaffhkksemckelloe", "fhea"];
+console.log(minWindowSubstring(test));
--- js/medium/triple-double.js
+++ js/medium/triple-double.js
@@ -1,20 +1,36 @@
 /**
- * Challenge 
- * Using the JavaScript language, have the function TripleDouble(num1,num2) 
- * take both parameters being passed, and return 1 if there is a straight 
- * triple of a number at any place in num1 and also a straight double of the 
- * same number in num2. For example: if num1 equals 451999277 and num2 equals 
- * 41177722899, then return 1 because in the first parameter you have the 
- * straight triple 999 and you have a straight double, 99, of the same number 
- * in the second parameter. If this isn't the case, return 0. 
- * Sample Test Cases 
- * Input:465555 & num2 = 5579 
- * 
- * Output:1 
- * 
- * 
- * Input:67844 & num2 = 66237 
- * 
- * Output:0 
+ * Challenge
+ * Using the JavaScript language, have the function TripleDouble(num1,num2)
+ * take both parameters being passed, and return 1 if there is a straight
+ * triple of a number at any place in num1 and also a straight double of the
+ * same number in num2. For example: if num1 equals 451999277 and num2 equals
+ * 41177722899, then return 1 because in the first parameter you have the
+ * straight triple 999 and you have a straight double, 99, of the same number
+ * in the second parameter. If this isn't the case, return 0.
+ * Sample Test Cases
+ * Input:465555 & num2 = 5579
+ *
+ * Output:1
+ *
+ *
+ * Input:67844 & num2 = 66237
+ *
+ * Output:0
  */
 
+function tripleDouble(num1, num2) {
+  for (let i = 0; i < 10; i++) {
+    const triple = "" + i + i + i;
+    const double = "" + i + i;
+    if (
+      num1.toString().indexOf(triple) !== -1 &&
+      num2.toString().indexOf(double) !== -1
+    ) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+// console.log(tripleDouble(465555, 5579));
+console.log(tripleDouble(67844, 66237));
