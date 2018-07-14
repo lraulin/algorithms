fe4fa9c0c6b3ef153694a82deb84e4e7ef8e3d64
= <leeraulin@gmail.com>
2018-07-14 12:05:08 -0400

    add lru crane

--- js/medium/longest-consecutive.js
+++ js/medium/longest-consecutive.js
@@ -69,3 +69,25 @@ const test = [4, 3, 8, 1, 2, 6, 100, 9];
 // const test = [6, 7, 3, 1, 100, 102, 6, 12];
 // const test = [5, 6, 1, 2, 8, 9, 7];
 console.log(longestConsecutive(test));
+
+// length of current sequence
+let currentCount = 1;
+// length of longest sequence
+let maxCount = 1;
+// compare values it indexes i and i + 1
+// for the last pair, i+1 is out of range, therefore undefined, therefore
+// will be treated as the end of a consecutive sequence
+for (let i = 0; i < sequence.length; i++) {
+  // if the next num equals current num + 1...
+  if (sequence[i + 1] === sequence[i] + 1) {
+    // the pairs are part of a consecutive sequence; increase the count
+    currentCount++;
+  } else {
+    // we've reached the end of a consecutive sequence
+    // update maxCount if appropriate
+    maxCount = currentCount > maxCount ? currentCount : maxCount;
+    // reset currentCount
+    currentCount = 1;
+  }
+}
+return maxCount;
--- js/medium/lru-crane.js
+++ js/medium/lru-crane.js
@@ -1,38 +1,61 @@
 /**
- * Challenge 
- * Using the JavaScript language, have the function LRUCache(strArr) take the 
- * array of characters stored in strArr, which will contain characters ranging 
- * from A to Z in some arbitrary order, and determine what elements still 
- * remain in a virtual cache that can hold up to 5 elements with an LRU cache 
- * algorithm implemented. For example: if strArr is ["A", "B", "C", "D", "A", 
- * "E", "D", "Z"], then the following steps are taken: 
- * 
- * (1) A does not exist in the cache, so access it and store it in the cache. 
- * (2) B does not exist in the cache, so access it and store it in the cache as 
- * well. So far the cache contains: ["A", "B"]. 
- * (3) Same goes for C, so the cache is now: ["A", "B", "C"]. 
- * (4) Same goes for D, so the cache is now: ["A", "B", "C", "D"]. 
- * (5) Now A is accessed again, but it exists in the cache already so it is 
- * brought to the front: ["B", "C", "D", "A"]. 
- * (6) E does not exist in the cache, so access it and store it in the cache: 
- * ["B", "C", "D", "A", "E"]. 
- * (7) D is accessed again so it is brought to the front: ["B", "C", "A", "E", 
- * "D"]. 
- * (8) Z does not exist in the cache so add it to the front and remove the 
- * least recently used element: ["C", "A", "E", "D", "Z"]. 
- * 
- * Now the caching steps have been completed and your program should return the 
- * order of the cache with the elements joined into a string, separated by a 
- * hyphen. Therefore, for the example above your program should return 
- * C-A-E-D-Z. 
- * Sample Test Cases 
- * Input:"A", "B", "A", "C", "A", "B" 
- * 
- * Output:"C-A-B" 
- * 
- * 
- * Input:"A", "B", "C", "D", "E", "D", "Q", "Z", "C" 
- * 
- * Output:"E-D-Q-Z-C" 
+ * Challenge
+ * Using the JavaScript language, have the function LRUCache(strArr) take the
+ * array of characters stored in strArr, which will contain characters ranging
+ * from A to Z in some arbitrary order, and determine what elements still
+ * remain in a virtual cache that can hold up to 5 elements with an LRU cache
+ * algorithm implemented. For example: if strArr is ["A", "B", "C", "D", "A",
+ * "E", "D", "Z"], then the following steps are taken:
+ *
+ * (1) A does not exist in the cache, so access it and store it in the cache.
+ * (2) B does not exist in the cache, so access it and store it in the cache as
+ * well. So far the cache contains: ["A", "B"].
+ * (3) Same goes for C, so the cache is now: ["A", "B", "C"].
+ * (4) Same goes for D, so the cache is now: ["A", "B", "C", "D"].
+ * (5) Now A is accessed again, but it exists in the cache already so it is
+ * brought to the front: ["B", "C", "D", "A"].
+ * (6) E does not exist in the cache, so access it and store it in the cache:
+ * ["B", "C", "D", "A", "E"].
+ * (7) D is accessed again so it is brought to the front: ["B", "C", "A", "E",
+ * "D"].
+ * (8) Z does not exist in the cache so add it to the front and remove the
+ * least recently used element: ["C", "A", "E", "D", "Z"].
+ *
+ * Now the caching steps have been completed and your program should return the
+ * order of the cache with the elements joined into a string, separated by a
+ * hyphen. Therefore, for the example above your program should return
+ * C-A-E-D-Z.
+ * Sample Test Cases
+ * Input:"A", "B", "A", "C", "A", "B"
+ *
+ * Output:"C-A-B"
+ *
+ *
+ * Input:"A", "B", "C", "D", "E", "D", "Q", "Z", "C"
+ *
+ * Output:"E-D-Q-Z-C"
  */
 
+function lruCrane(array) {
+  let cache = [];
+  for (let letter of array) {
+    const cacheIndex = cache.indexOf(letter);
+    const cacheFull = cache.length >= 5;
+    if (!cacheFull && cacheIndex === -1) {
+      // Cache is not full and letter is not in the cache, so add it to the cache.
+      cache.push(letter);
+    } else if (cacheIndex > -1) {
+      // Letter is in the cache, so bring it to the front (whether or not cache is full).
+      cache.splice(cacheIndex, 1);
+      cache.push(letter);
+    } else if (cacheFull) {
+      // Cache is full and letter is not in cache; remove first (oldest) element and add letter.
+      cache.shift();
+      cache.push(letter);
+    }
+  }
+  return cache.join("-");
+}
+
+const test = ["A", "B", "C", "D", "A", "E", "D", "Z"];
+console.log(lruCrane(test));
