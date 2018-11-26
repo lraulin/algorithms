615410d2328057df3572ad0735fe3b864efcb422
Lee Raulin <leeraulin@gmail.com>
2018-11-26 15:38:00 -0500

    CamelCase

---
+++ csharp/CamelCase/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CamelCase
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("camel case".CamelCase());
+        }
+
+        public static string CamelCase(this string str)
+        {
+            string[] arr = str.Split(' ');
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr[i][0].ToString().ToUpper() + arr[i].Substring(1);
+            }
+            return String.Join("", arr);
+        }
+    }
+}
