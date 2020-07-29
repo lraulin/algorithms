c4b809d8db3be1dbebdbdb3a9f1721223558a41c
Lee Raulin <leeraulin@gmail.com>
2020-07-29 16:03:27 -0400

    tree constructor

--- js/medium/tree-constructor.js
+++ js/medium/tree-constructor.js
@@ -1,29 +1,75 @@
 /**
- * Challenge 
- * Using the JavaScript language, have the function TreeConstructor(strArr) 
- * take the array of strings stored in strArr, which will contain pairs of 
- * integers in the following format: (i1,i2), where i1 represents a child node 
- * in a tree and the second integer i2 signifies that it is the parent of i1. 
- * For example: if strArr is ["(1,2)", "(2,4)", "(7,2)"], then this forms the 
- * following tree: 
- * 
- * 
- * 
- * which you can see forms a proper binary tree. Your program should, in this 
- * case, return the string true because a valid binary tree can be formed. If a 
- * proper binary tree cannot be formed with the integer pairs, then return the 
- * string false. All of the integers within the tree will be unique, which 
- * means there can only be one node in the tree with the given integer value. 
- * Sample Test Cases 
- * Input:"(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)" 
- * 
- * Output:"true" 
- * 
- * 
- * Input:"(1,2)", "(3,2)", "(2,12)", "(5,2)" 
- * 
- * Output:"false" 
- * 
- * 
+ * Challenge
+ * Using the JavaScript language, have the function TreeConstructor(strArr)
+ * take the array of strings stored in strArr, which will contain pairs of
+ * integers in the following format: (i1,i2), where i1 represents a child node
+ * in a tree and the second integer i2 signifies that it is the parent of i1.
+ * For example: if strArr is ["(1,2)", "(2,4)", "(7,2)"], then this forms the
+ * following tree:
+ *
+ *
+ *
+ * which you can see forms a proper binary tree. Your program should, in this
+ * case, return the string true because a valid binary tree can be formed. If a
+ * proper binary tree cannot be formed with the integer pairs, then return the
+ * string false. All of the integers within the tree will be unique, which
+ * means there can only be one node in the tree with the given integer value.
+ * Sample Test Cases
+ * Input:"(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)"
+ *
+ * Output:"true"
+ *
+ *
+ * Input:"(1,2)", "(3,2)", "(2,12)", "(5,2)"
+ *
+ * Output:"false"
+ *
+ *
  */
 
+function TreeConstructor(strArr) {
+  strArr = strArr
+    .map((x) =>
+      x
+        .slice(1, x.length - 1)
+        .split(",")
+        .map((x) => Number.parseInt(x))
+    )
+    .sort((a, b) => b[0] - a[0]);
+  console.log(strArr);
+
+  let tree = {};
+
+  for (let [childKey, parentKey] of strArr) {
+    console.log(`[${childKey},${parentKey}]`);
+    if (!tree[parentKey]) tree[parentKey] = createNode(null);
+
+    const parent = tree[parentKey];
+    if (parent.left !== null && parent.right !== null) return false;
+
+    if (!tree[childKey]) tree[childKey] = createNode(parent);
+    else tree[childKey].parent = parent;
+
+    if (childKey < parentKey) {
+      if (parent.left) return false;
+      parent.left = tree[childKey];
+    } else if (childKey > parentKey) {
+      if (parent.right) return false;
+      parent.right = tree[childKey];
+    }
+  }
+
+  return (
+    Array.from(Object.values(tree)).filter(({ parent }) => parent === null)
+      .length === 1
+  );
+}
+
+const createNode = (parent, left = null, right = null) => ({
+  parent,
+  left,
+  right,
+});
+
+const arr = ["(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)"];
+console.log(TreeConstructor(arr));
